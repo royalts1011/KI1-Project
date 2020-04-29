9cb5826f65543eda9a54710e22b0b852bdc027c0
Jesse <jesse.kruse@student.uni-luebeck.de>
2020-04-29 15:26:24 +0200

    laeuft grob..

--- Aufgabe_2/antFarm.m
+++ Aufgabe_2/antFarm.m
@@ -15,9 +15,9 @@ load('ant.mat');
 % Setze Parameter.
 n = 10;
 % TODO: Setzt die Parameter eures Genetischen Algorithmus.
-nSurv = 
-nMutations = 
-maxIter = 
+nSurv = 3;
+nMutations = 4;
+maxIter = 8000;
 
 % Initialisierung.
 P = zeros(n, x, y);
@@ -26,14 +26,25 @@ fitness = zeros(n, 1);
 % Generiere Startpopulation.
 % TODO: Kommentiert/Unkommentiert eine der Zeilen, um eine der Startpopulationen zu erhalten.
 for i = 1:n
-%     P(i, :, :) = round(rand(x, y));
+      P(i, :, :) = round(rand(x, y));
 %     P(i, :, :) = antdummy;
 end
 
 % Initiale Fitness.
 % TODO: Berechnet Fitness der Startpopulation.
+
+ant = double(ant);
 for i = 1:n
-    fitness(i) = 
+    summe = 0;
+    for j = 1:x
+        for l = 1:y
+            if (ant(j,l) == P(i,j,l))
+                summe = summe +1;
+            end
+        end
+    end
+    fitness(i) = summe/(x*y);
+    
 end
 
 % Visualisierung der Startpopulation.
@@ -49,20 +60,80 @@ for k = 1:maxIter
     
     % Selektion.
     % TODO: Selektiert die besten Individuen.
+    best_indivs = zeros(1,nSurv);
+    for i = 1:nSurv
+        [max_val, tmp_idx] = max(fitness);
+        best_indivs(i) = tmp_idx;
+        % null setzen, damit idx nicht doppelt als bester vorkommt (vtl
+        % lieber (-1)
+        fitness(tmp_idx) = 0;
+    end
  
     
     % Rekombination.
     % TODO: Generiert neue Individuen aus Überlebenden durch Rekombination.
-
+    
+    % 6 rekombinationene vom besten mit allen
+    P_old = P;
+    
+    for i = 1:nSurv
+        
+        for diff = 2:nSurv
+            for j = 1:x
+                if(mod(j,2) == 0)
+                    P(i,j,:) =  P_old(best_indivs(1),j,:);
+                else                  
+                    P(i,j,:) =  P_old(best_indivs(diff),j,:);
+                end
+            end
+        end
+        
+    end
+    
+    % übrige rekombinationen zweitbester bis voll
+   
+    for i = (nSurv + 1):size(P,1)
+        
+        for diff = 3:nSurv
+            for j = 1:x
+                if (mod(j,2) == 0)
+                    P(i,j,:) = P_old(best_indivs(2),j,:);
+                else
+                    P(i,j,:) = P_old(best_indivs(diff),j,:);  
+                end
+            end
+        end
+        
+    end
+    
     
     % Mutation.
     % TODO: Mutiere zufällig die neuen Individuen.
-
+    
+    for i = 1:size(P,1)
+        num_muts = nMutations;
+        for j = 0:num_muts
+            idx_x = round(rand() * 197 + 1);
+            idx_y = round(rand() * 159 + 1);
+            
+            P(i, idx_x, idx_y) = ~P(i, idx_x, idx_y);
+            
+        end
+    end
+    
     
     % Fitness-Update.
     % TODO: Berechne Fitness der neuen Generation.
     for i = 1:n
-        fitness(i) = 
+        summe = 0;
+        for j = 1:x
+            for l = 1:y
+                if (ant(j,l) == P(i,j,l))
+                    summe = summe + 1;
+                end
+            end
+        end
+    fitness(i) = summe/(x*y);
     end
     
     % Abbruchkriterium, falls Population sehr gut ist.
