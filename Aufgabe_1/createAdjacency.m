84c5ea1fea8c57c22cf2c3d7dbc842ce5ef2c840
Nils Loose <nils.loose@OSX298-MBP.local>
2020-05-02 18:00:44 +0200

    Kommentare bei createAd hinzugefügt

--- Aufgabe_1/createAdjacency.m
+++ Aufgabe_1/createAdjacency.m
@@ -1,17 +1,20 @@
 %Berechnen Sie hier Ihre Adjazenzmatrix.
 
-size = 4096;
-A = zeros(size, size);
-
-for n = 1:size
+size = 4096;                    % 2^12 = 4096 mögliche Bilder / Knoten
+A = zeros(size, size);          %Initialisierung der Adjazenzmatrix
+    
+for n = 1:size                  %Iterieren über alle möglichen Knoten
         
-   n_bin = de2bi(n-1,12);
-   for i = 1:12
-      m_bin = n_bin;
-      m_bin(i) = ~m_bin(i);
+   n_bin = de2bi(n-1,12);       %Umwandlung von Dezimal in eine 12-Stellige Binärzahl
+   for i = 1:12                 %Iterieren über alle bit-Positionen
+      m_bin = n_bin;            %und flippen immer genau ein Bit / ein Pixel
+      m_bin(i) = ~m_bin(i);     %Hier wird das i-te bit geflippt
       A(n,bi2de(m_bin)+1) = 1;
       A(bi2de(m_bin)+1,n) = 1;
    end
     
 end
+                                %Wir rechnen +1 bei den Indizes  der Matrix
+                                %da diese bei eins und nicht wie unsere
+                                %Knoten bei null anfängt
 save('Adjacency.mat','A')
