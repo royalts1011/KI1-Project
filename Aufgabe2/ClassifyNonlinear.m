9990442665e30bdb7ef3c720420c1339769b8234
Falco Lentzsch <falco.lentzsch@student.uni-luebeck.de>
2020-05-23 09:09:27 +0200

    Aufgabe2 online

---
+++ Aufgabe2/ClassifyNonlinear.m
@@ -0,0 +1,66 @@
+%KI Projekt 2 Aufgabe 2
+%--------------------------------------------------------------------------
+%Unterscheiden Sie gute von nicht so guten Weinen. 
+%Ein Wein zählt als gut wenn er einen Score von 7 oder mehr erreicht.
+
+%Lade Datensatz
+load('Data.mat');
+
+
+%Beipsielhaftes Training einer SVR. Hier wird ein radial basis function (rbf) Kernel
+%verwendet.
+C=1;
+gamma=10;
+
+%training
+SVMModel_nonlinear=fitcsvm(X_train,y_train,'BoxConstraint',C,'KernelFunction','rbf','KernelScale',gamma);
+
+%test
+y_pred=predict(SVMModel_nonlinear,X_test);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
