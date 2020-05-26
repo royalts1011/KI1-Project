3c3a945b24415c3654d60bebcec1efa59b9c3601
Nils Loose <nils.loose@OSX298-MBP.local>
2020-05-26 14:15:25 +0200

    added skeleton for task 3

---
+++ Aufgabe_3/Aufgabe1/predict_kNN.m
@@ -0,0 +1,11 @@
+function [Y_predict] = predict_kNN(X_train,Y_train,X_test, k)
+%In dieser Funktion wird ein k-Nearest-Neighbor Klassifikator genutzt, um auf
+%Basis der Beobachtungen X_train und Y_train eine Klassifikation für X_test
+%zu treffen. k beschreibt die Anzahl der betrachteten Nachbarn.
+
+
+
+
+
+end
+
