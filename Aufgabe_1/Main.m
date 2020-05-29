fbe8b630ee1fe38bd25d20b21eddd87fc78a048d
Nils Loose <nils.loose@OSX298-MBP.local>
2020-05-29 16:08:58 +0200

    Aufgabe 1 fertig nur noch kommentare und schreiben

--- Aufgabe_1/Main.m
+++ Aufgabe_1/Main.m
@@ -10,22 +10,44 @@
 load('Dataset.mat');
 %load('DatasetLarge.mat');
 
-[k_best,best_acc] = optimise_k(1,200,X_train,Y_train,X_test,Y_test);
-k_best
-best_acc
+[knn_k_best,knn_acc,knn_pred] = optimise_k(1,200,X_train,Y_train,X_test,Y_test);
+[nb_acc,nb_pred] = nb_accuracy(X_train, Y_train, Y_test, X_test);
+nb_acc
+knn_k_best
+knn_acc
+subplot(2,2,1);
+gscatter(X_train(:,1),X_train(:,2),Y_train,'rgb','osd');
+xlabel('Feature 1');
+ylabel('Feature 2');
 
-figure()
-for i=1:size(X_train,1)         %1000 is the length of x_vector and y_vector
-   plot(X_train(i,1), X_train(i,2), 'o')
-   hold on
-   axis([-3 7 -3 7]);
-   set(gca, 'ydir', 'reverse', 'xaxislocation', 'top')
-end
+title("X_{train} with labels");
+
+subplot(2,2,2);
+gscatter(X_test(:,1),X_test(:,2),Y_test,'rgb','osd');
+xlabel('Feature 1');
+ylabel('Feature 2');
+
+title("X_{test} with labels");
+
+subplot(2,2,3);
+gscatter(X_test(:,1),X_test(:,2),knn_pred,'rgb','osd');
+xlabel('Feature 1');
+ylabel('Feature 2');
+
+title("X_{test} evaluated with knn");
+
+subplot(2,2,4);
+gscatter(X_test(:,1),X_test(:,2),nb_pred,'rgb','osd');
+xlabel('Feature 1');
+ylabel('Feature 2');
+title("X_{test} evaluated with nb");
 
 
 
 
-function [k_best,best_acc] = optimise_k(k_begin,k_end,X_train,Y_train,X_test,Y_test)
+
+%gscatter(X_train(:,1),X_train(:,2),'rgb','osd');
+function [k_best,best_acc,knn_pred] = optimise_k(k_begin,k_end,X_train,Y_train,X_test,Y_test)
     k_best =  -1;
     best_acc = -1;
     acc = 0;
@@ -39,19 +61,21 @@ function [k_best,best_acc] = optimise_k(k_begin,k_end,X_train,Y_train,X_test,Y_t
         if acc > best_acc
             best_acc = acc;
             k_best = iter;
+            knn_pred = Y_pred;
+            
         end
     end
-   
+
 end
 
-
-
-
-
-
-
-
-
+function [acc,Y_pred] = nb_accuracy(X_train, Y_train, Y_test, X_test)
+    Y_pred = predict_NB(X_train,Y_train,X_test);
+    acc = 0;
+    for pred=1:size(Y_test,1)
+        acc = acc + 1 - abs(Y_pred(pred) - Y_test(pred));
+    end
+    acc = acc/size(Y_test,1);
+end
 
 
 
