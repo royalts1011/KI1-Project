8dceabed325ca2b593afbbc06a3c5fd02d44d933
Nils Loose <nils.loose@OSX298-MBP.local>
2020-07-12 20:04:02 +0200

    ...

--- Aufgabe2/TransferLearning.m
+++ Aufgabe2/TransferLearning.m
@@ -10,7 +10,7 @@
 
 %Lade Bilddaten in imageDatstore-Objekt.
 %myfolder='/meinPfad/zudenBildern/Bilder';
-myfolder='/Users/jannis/Downloads/p3_funktionsruempfe-2/Aufgabe2/Bilder';
+myfolder='/Users/nils.loose/Documents/Künstliche Intelligenz/KI1-Project/Aufgabe2/Bilder';
 imds = imageDatastore(myfolder,'FileExtensions',{'.jpg'},'IncludeSubfolders',true,'LabelSource','foldernames');
 
 %Teile in Trainings- und Testdaten ein
@@ -18,30 +18,39 @@ numTrainFiles=0.75; %Nutze 75% der Daten zum Training
 [imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
 
 %TODO: Definiere die Netzarchitektur
-layers = [];
-
+net = resnet101; 
+numClasses = numel(categories(imdsTrain.Labels))
+lgraph = layerGraph(net)
+newFCLayer = fullyConnectedLayer(numClasses,'Name','new_fc','WeightLearnRateFactor',10,'BiasLearnRateFactor',10); 
+lgraph = replaceLayer(lgraph,'fc1000',newFCLayer); 
+newClassLayer = classificationLayer('Name','new_classoutput'); 
+lgraph = replaceLayer(lgraph,'ClassificationLayer_predictions',newClassLayer);
 %Parameter des Lernverfahrens. 'MaxEpochs' gibt die Anzahl der zu
 %trainierenden Epochen an und definiert dadurch, wie lange das Netz
 %traininert wird.
-options = trainingOptions('adam', ...
+options = trainingOptions('sgdm', ...
     'InitialLearnRate',0.01, ...
-    'MaxEpochs',50, ...
+    'MaxEpochs',10, ...
     'Shuffle','every-epoch', ...
     'ValidationData',imdsValidation, ...
-    'ValidationFrequency',10, ...
+    'ValidationFrequency',1, ...
     'Verbose',false, ...
-    'Plots','training-progress');
+    'Plots','training-progress');%, ...
+    % 'MiniBatchSize', 64);
 
 %Definiere das Netz anhand der zuvor definierten Layer und starte Training.
-net = trainNetwork(imdsTrain,layers,options);
+myNet = trainNetwork(imdsTrain,lgraph,options);
 
 %Evaluation. YPred beinhaltet die praedizierten Label, YTrue die
 %tatsaechlichen.
-YPred = classify(net,imdsValidation);
+YPred = classify(myNet,imdsValidation);
 YTrue = imdsValidation.Labels;
 
 %TODO: Evaluation und Output
 
+YPred
+YTrue
+accuracy = sum(YPred == YTrue)/numel(YTrue)
 
 
 
